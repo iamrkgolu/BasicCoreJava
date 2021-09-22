7aef26f764943e25f9911ef527ff42b5ca1eaf1a
Rahul Kumar <iamrkgolu@gmail.com>
2021-09-22 19:26:44 +0530

    Deleted
    hhii

--- Account.java
+++
@@ -1,9 +0,0 @@
-package Basic;
-
-public class Account {
-	public static void main(String[] args) {
-		int Arr []= {2,4,6,8};
-		String s=Arr.toString();
-		System.out.println(s);
-	}
-}
--- StringExample1.java
+++
@@ -1,5 +0,0 @@
-package com.basicString.org;
-
-public class StringExample1 {
-
-}
